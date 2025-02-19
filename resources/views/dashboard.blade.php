8dfd66eec4bb92c19b71ffb590b1416bc1148d84
LozzDev <lozzdeveloper@gmail.com>
2025-02-19 14:43:05 +0100

    Update application logo and enhance dashboard layout with custom styles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,9 @@
         </h2>
     </x-slot>
 
+    <!-- Estilos personalizados -->
+    
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -14,4 +17,6 @@
             </div>
         </div>
     </div>
+
 </x-app-layout>
+
