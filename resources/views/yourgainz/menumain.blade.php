7f434700296114956f1c26c4e2aae144eadf5935
JairoAlexandro <jairosf3113@gmail.com>
2025-02-19 20:51:30 +0100

    Refactor user profile display in menumain view for improved layout and styling

--- resources/views/yourgainz/menumain.blade.php
+++ resources/views/yourgainz/menumain.blade.php
@@ -6,24 +6,19 @@
                 <img src="https://res.cloudinary.com/dgzgzx9ov/image/upload/v1739966822/YourGainz_Logo_Completo_1_gsl94c.png"
                     alt="YourGainz Logo" class="h-60 md:h-60 lg:h-180">
             </div>
-            <div class="text-black p-6 rounded-lg relative md:bg-[#ffffffd2]">
-
-
-                <div
-                    class="  p-4 rounded-lg hidden md:flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4">
-                    <img src="https://res.cloudinary.com/dgzgzx9ov/image/upload/v1739966822/YourGainz_Logo_Completo_1_gsl94c.png"
-                        class=" w-48 h-48 rounded-full" alt="User">
-                    <div class="text-center md:text-left">
-                        <h3 class="text-lg font-semibold">{{ Auth::user()->name }}</h3>
-                        <p class="text-sm">Peso: {{ Auth::user()->weight }}</p>
-                        <p class="text-sm">Altura: {{ Auth::user()->height }}</p>
-                        <p class="text-sm">IMC: Poner funcion</p>
-                    </div>
+            <div class="hidden md:flex justify-center items-center p-6 rounded-lg relative">
+                <div class="relative w-48 h-48 lg:w-64 lg:h-64 rounded-full overflow-hidden border-8 border-[#fff]">
+                    <img src="https://res.cloudinary.com/dgzgzx9ov/image/upload/v1739966822/YourGainz_Logo_Completo_1_gsl94c.png" 
+                        class="w-full h-full object-cover rounded-full" alt="User">
                 </div>
-
-
-
+                <div class="p-4 bg-[#ffffffd3] rounded-xl text-black shadow-lg ml-[-108px] lg:ml-[-128px] pl-20 lg:pl-24 h-48 w-3/4 lg:h-64 flex flex-col justify-center">
+                    <h3 class="text-xl lg:text-2xl font-semibold ml-12">{{ Auth::user()->name }}</h3>
+                    <p class="text-lg lg:text-xl ml-12">Peso: {{ Auth::user()->weight }}</p>
+                    <p class="text-lg lg:text-xl ml-12">Altura: {{ Auth::user()->height }}</p>
+                    <p class="text-lg lg:text-xl ml-12">IMC: Poner función</p>
+                </div>
             </div>
+            
             <div class=" flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-4 md:mt-6">
                 <button
                     class="bg-[#FFF700] hover:bg-[#e2dc2e] text-gray-900 font-bold py-2 px-4 rounded-3xl flex items-center justify-center w-full md:w-40">
